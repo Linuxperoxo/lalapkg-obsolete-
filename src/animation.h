f3af9d5b41911e5f1a0c72b548d0820fea3af6d2
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-07-04 21:43:00 +0000

    Update animation.h

--- src/animation.h
+++ src/animation.h
@@ -1,3 +1,9 @@
+//==========================================|
+//   FILE: animation.h                      |
+//   AUTHOR: Linuxperoxo                    |
+//   COPYRIGHT: (c) 2024 per Linuxperoxo.   |
+//==========================================/
+
 #ifndef ANIMATION_H
 #define ANIMATION_H
 
