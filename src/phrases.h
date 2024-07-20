e9151609fd9353dec08776644b79cfdc37233197
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-07-20 00:25:07 +0000

    Add files via upload

---
+++ src/phrases.h
@@ -0,0 +1,8 @@
+#ifndef PHRASES_H
+#define PHRASES_H
+
+#include <string>
+
+std::string Phrases(char type);
+
+#endif
---
+++ src/random.h
@@ -0,0 +1,9 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include "random.cc"
+
+template <typename T>
+T randomNum(T min, T max);
+
+#endif
