fc07fb5d8e885e4fccdea4718ed6b6300b83f6f9
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-07-20 03:15:15 +0000

    Atualizar o animation.cc

--- src/animation.cc
+++ src/animation.cc
@@ -4,6 +4,8 @@
 //   COPYRIGHT: (c) 2024 per Linuxperoxo.   |
 //==========================================/
 
+//==========================================================| LIBS
+
 #include <cctype>
 #include <iostream>
 #include <chrono>
@@ -11,15 +13,19 @@
 #include <string>
 #include <atomic>
 
+//==========================================================| MY HEADERS
+
 #include "animation.h"
 #include "color.h"
 #include "phrases.h"
 
+//==========================================================| FUNCTIONS
 
 void animate(std::atomic<bool>& done, std::string msg, char type){
 
   const std::vector<std::string> animation[] = {
     
+      // Vetor de cada frame da animação 
       "c o o o o o o o o o",
       "C o o o o o o o o o",
       "--c o o o o o o o o",
@@ -44,31 +50,40 @@ void animate(std::atomic<bool>& done, std::string msg, char type){
 
   };
   
+  // Pegando uma frase aleatória
   const std::string phrase = Phrases(type);
 
+  // Ajustando delay de troca dos frames 
   const int delay = 150;
 
+  // Mostrando mensagem passada na função 
   std::cout << msg << std::endl;
 
+  // While responsável por manter rodando a animação 
   while(!done){
 
+    // interando o vetor das animações 
     for(const std::string& frames : animation){
 
       std::cout << "\r" GREEN "* " NC << phrase << NC BLUE " [" NC << frames << BLUE "]" NC;
       std::cout.flush();
 
+      // Verificando se a tarefa foi concluída.
+      // Caso não tenha essa verificação vai passar por todos os frames antes de sair
       if(done){
 
         break;
 
       }
 
+      // Timer para trocar de frame
       std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 
     }    
 
   }
 
+  // Quebrando a linha para a saída ficar mais bonitinha :D
   std::cout << std::endl;
 
 }
