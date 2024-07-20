b06673b1f0fe90649dadb13d4448109d7f4d203c
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-07-20 03:17:15 +0000

    Atualizar o animation.h

--- src/animation.h
+++ src/animation.h
@@ -5,11 +5,14 @@
 //==========================================/
 
 #ifndef ANIMATION_H
-#define ANIMATION_H
+#define ANIMATION_
+
+//==========================================================| LIBS
 
 #include <atomic>
 #include <string>
 
+// Declarando função 
 void animate(std::atomic<bool> &done, std::string msg, char type = 'z');
 
 #endif 
